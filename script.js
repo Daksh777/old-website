bf5d3687236ca4d5936330f0fcf724ced29a7e38
Daksh P. Jain <contact@dakshpjain.eu.org>
2020-07-16 10:08:22 +0530

    Experimental JS minifying

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-window.addEventListener("load", event => {
-  var discord = document.getElementsByClassName("n05")[0];
-  var discordLabel = document.querySelectorAll(".n05 .label")[0];
-  discord.addEventListener("click", showDiscordDetails, false);
-
-  function showDiscordDetails() {
-    discord.classList.add("discordClick");
-    discordLabel.classList.add("discordClick");
-    hideOnClickOutside(discord);
-  }
-
-  function hideOnClickOutside(element) {
-    const outsideClickListener = event => {
-      if (!element.contains(event.target) && isVisible(element)) {
-        discordLabel.classList.remove("discordClick");
-        discord.classList.remove("discordClick");
-        removeClickListener();
-      }
-    };
-    const removeClickListener = () => {
-      document.removeEventListener("click", outsideClickListener);
-    };
-    document.addEventListener("click", outsideClickListener);
-  }
-
-  const isVisible = elem =>
-    !!elem &&
-    !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length);
-});
+window.addEventListener("load",event=>{var discord=document.getElementsByClassName("n05")[0];var discordLabel=document.querySelectorAll(".n05 .label")[0];discord.addEventListener("click",showDiscordDetails,false);function showDiscordDetails(){discord.classList.add("discordClick");discordLabel.classList.add("discordClick");hideOnClickOutside(discord);}
+function hideOnClickOutside(element){const outsideClickListener=event=>{if(!element.contains(event.target)&&isVisible(element)){discordLabel.classList.remove("discordClick");discord.classList.remove("discordClick");removeClickListener();}};const removeClickListener=()=>{document.removeEventListener("click",outsideClickListener);};document.addEventListener("click",outsideClickListener);}
+const isVisible=elem=>!!elem&&!!(elem.offsetWidth||elem.offsetHeight||elem.getClientRects().length);});

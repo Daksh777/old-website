9fbcdab95d75867d719c0b245ef33061b46e83b2
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2020-10-14 21:25:52 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -788,30 +788,13 @@
     }),
     new m().init({ id: "gallery01", navigation: !0 });
 })();
-// discord
+
 window.addEventListener("load", event => {
   var discord = document.getElementsByClassName("n05")[0];
   var discordLabel = document.querySelectorAll(".n05 .label")[0];
   discord.addEventListener("click", showDiscordDetails, false);
   function showDiscordDetails() {
-    discord.classList.add("discordClick");
-    discordLabel.classList.add("discordClick");
-    hideOnClickOutside(discord);
+    discordLabel.textContent = 'Copied to clipboard'
+    setTimeout(() => discordLabel.textContent = 'Daksh777#7688', 2000)
   }
-  function hideOnClickOutside(element) {
-    const outsideClickListener = event => {
-      if (!element.contains(event.target) && isVisible(element)) {
-        discordLabel.classList.remove("discordClick");
-        discord.classList.remove("discordClick");
-        removeClickListener();
-      }
-    };
-    const removeClickListener = () => {
-      document.removeEventListener("click", outsideClickListener);
-    };
-    document.addEventListener("click", outsideClickListener);
-  }
-  const isVisible = elem =>
-    !!elem &&
-    !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length);
 });
